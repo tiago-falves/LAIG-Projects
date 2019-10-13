8b196bdb8bbb45750bce06cbb7351dcc186b1005
tiago-falves <tiago.falves98@gmail.com>
2019-10-13 18:44:12 +0100

    added textures fixed inheritance towers

--- tp1/XMLscene.js
+++ tp1/XMLscene.js
@@ -137,6 +137,7 @@ class XMLscene extends CGFscene {
 
         this.setGlobalAmbientLight(this.graph.ambient[0], this.graph.ambient[1], this.graph.ambient[2], this.graph.ambient[3]);
 
+        //Comentar init Cameras
         this.initCameras();
         this.initLights();
 
