a08e38ae1bc5fd7f4abbc2c71d2473bda84866de
SALajes <sofialajes98@gmail.com>
2019-11-22 21:11:38 +0000

    Corrected xml file name

--- tp2/main.js
+++ tp2/main.js
@@ -30,7 +30,7 @@ main=function()
 	// get file name provided in URL, e.g. http://localhost/myproj/?file=myfile.xml 
 	// or use "demo.xml" as default (assumes files in subfolder "scenes", check MySceneGraph constructor) 
 	
-    var filename=getUrlVars()['file'] || "LAIG_TP1_T6_G10_v03.xml";
+    var filename=getUrlVars()['file'] || "LAIG_TP2_T6_G10.xml";
 
 	// create and load graph, and associate it to scene. 
 	// Check console for loading errors
